a401c419eab024e005f933824f3f1edb2502bc64
Dmitry Romashov <framebassman@gmail.com>
2019-06-20 13:47:39 +0000

    fix errors

--- Chat.Web/ClientApp/src/components/Chat.jsx
+++ Chat.Web/ClientApp/src/components/Chat.jsx
@@ -13,7 +13,27 @@ class Chat extends Component {
         hubConnection: null,
       };
     }
-  
+    
+    componentDidMount = () => {
+        const nick = window.prompt('Your name:', 'John');
+        const hubConnection = new signalR.HubConnectionBuilder()
+            .withUrl("/signalr", { useDefaultPath: false })
+            .build();
+    
+        this.setState({ hubConnection, nick }, () => {
+            this.state.hubConnection
+              .start()
+              .then(() => console.log('Connection started!'))
+              .catch(err => console.log('Error while establishing connection :('));
+      
+            this.state.hubConnection.on('sendToAll', (nick, receivedMessage) => {
+              const text = `${nick}: ${receivedMessage}`;
+              const messages = this.state.messages.concat([text]);
+              this.setState({ messages });
+            });
+          });
+    }
+
     render() {
       return <div>Here goes chat</div>;
     }
