e45db4a6cfd39d9d2af6b5b89492f469d0671d6a
Dmitry Romashov <framebassman@gmail.com>
2019-06-21 16:29:59 +0300

    add logging

--- Chat.Web/Model/ChatHub.cs
+++ Chat.Web/Model/ChatHub.cs
@@ -1,12 +1,21 @@
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Chat.Web.Model
 {
     public class ChatHub : Hub
     {
+        private readonly ILogger<ChatHub> _log;
+
+        public ChatHub(ILogger<ChatHub> log)
+        {
+            _log = log;
+        }
+
         public async Task NewMessage(string user, string message)
         {
+            _log.LogInformation($"Send message: {message} to user: {user}");
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
