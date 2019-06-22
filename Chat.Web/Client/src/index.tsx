a3e9abe0a2e6020bc97c0f58d1444eae19e2e415
Dmitry Romashov <framebassman@gmail.com>
2019-06-22 11:55:48 +0300

    add YandexMetrica to chat

--- Chat.Web/Client/src/index.tsx
+++ Chat.Web/Client/src/index.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { YandexMetrica } from './YandexMetrica';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <App />
+    <YandexMetrica accounts={[52190806]} />
+  </div>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
