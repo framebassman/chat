eb7a87358a0f251163dfc45030d5f7a58f7aea22
Dmitry Romashov <framebassman@gmail.com>
2020-06-15 23:32:35 +0300

    add nunit project

---
+++ Chat.Tests.Integration/Model/MainPage.cs
@@ -0,0 +1,27 @@
+using Atata;
+using OpenQA.Selenium;
+using _ = Chat.Tests.Integration.Model.MainPage;
+
+namespace Chat.Tests.Integration.Model
+{
+    [Url("")]
+    public class MainPage : Page<_>
+    {
+        [FindById("input")]
+        public TextInput<_> Input { get; private set; }
+        
+        [FindById("send")]
+        public Button<_> Send { get; private set; }
+
+        public _ LoginAs(string name)
+        {
+            IAlert alert = Driver.SwitchTo().Alert();
+            alert.SendKeys(name); // Note that SendKeys doesn't work in Chrome for a long time. Works in Firefox for example.
+            alert.Accept();
+
+            Driver.SwitchTo().DefaultContent();
+
+            return Owner;
+        }
+    }
+}
---
+++ Chat.Tests.Integration/Tests/UITestFixture.cs
@@ -0,0 +1,30 @@
+using Atata;
+using NUnit.Framework;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace Chat.Tests.Integration.Tests
+{
+    [TestFixture]
+    public class UITestFixture
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            AtataContext.Configure()
+                .UseChrome()
+                .UseBaseUrl("http://localhost:5000")
+                .UseNUnitTestName()
+                .AddNUnitTestContextLogging()
+                .LogNUnitError()
+                .Build();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            AtataContext.Current.CleanUp();
+        }
+    }
+}
