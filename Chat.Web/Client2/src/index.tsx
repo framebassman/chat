999885382a82882a557817d7ea4ab3991102e8eb
Dmitry Romashov <framebassman@gmail.com>
2022-08-28 10:30:57 +0200

    Fix index.tsx

--- Chat.Web/Client2/src/index.tsx
+++ Chat.Web/Client2/src/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { sentry_dns, env } from './environment';
+import { YandexMetrica } from './YandexMetrica';
+import * as Sentry from '@sentry/browser';
+import './index.css';
+
+Sentry.init({
+  dsn: sentry_dns,
+  maxBreadcrumbs: 50,
+  environment: env(),
+  debug: env() === 'PRODUCTION' ? false : true,
+  release: process.env.SENTRY_RELEASE,
+  beforeBreadcrumb(breadcrumb: any, hint: any) {
+    if (breadcrumb.category === 'ui.click') {
+      const { target } = hint.event;
+      if (target.ariaLabel) {
+        breadcrumb.message = target.ariaLabel;
+      }
+    }
+    return breadcrumb;
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +30,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <YandexMetrica accounts={[52190806]} />
   </React.StrictMode>
 );
 
