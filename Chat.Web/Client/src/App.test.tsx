a3a09fb2399cfb41fd5d1c84e5eb0206454e8e0d
Dmitry Romashov <framebassman@gmail.com>
2022-08-28 13:06:57 +0200

    Fix build

--- Chat.Web/Client/src/App.test.tsx
+++ Chat.Web/Client/src/App.test.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+window.prompt = jest.fn();
+
 test('renders learn react link', () => {
-  const jsdomPrompt = window.prompt;  // remember the jsdom alert
-  window.prompt = () => {};  // provide an empty implementation for window.alert
   render(<App />);
   const linkElement = screen.getByPlaceholderText(/Напишите сообщение.../i);
   expect(linkElement).toBeInTheDocument();
-  window.prompt = jsdomPrompt;
 });
